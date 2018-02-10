538941ac57cd8d40f599dc203ba82fa4d4185474
vishal-pandey <pandeyvishal25@yahoo.com>
2018-02-10 22:43:02 +0530

    s

--- login/index.php
+++ login/index.php
@@ -1,1 +1,13 @@
-Hello
+<?php
+	use \Firebase\JWT\JWT;
+	require '../vendor/autoload.php';
+	include '../config.php';
+	
+	$json = file_get_contents("php://input");
+	if ($json) {
+		$obj = json_decode($json);
+		echo $obj->id;
+		echo $obj->pwd;
+	}
+
+?>

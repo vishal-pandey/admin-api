2b4308ecb525ff3f6fa6ad6bedf809ffec3d3851
vishal-pandey <pandeyvishal25@yahoo.com>
2018-02-10 23:09:28 +0530

    s

--- login/index.php
+++ login/index.php
@@ -1,13 +1,22 @@
 <?php
 	use \Firebase\JWT\JWT;
 	require '../vendor/autoload.php';
-	include '../config.php';
 	
 	$json = file_get_contents("php://input");
 	if ($json) {
 		$obj = json_decode($json);
 		echo $obj->id;
 		echo $obj->pwd;
+
+		if ($obj->id == 'admin' && $obj->pwd == 'abc@123') {
+			$key = "Vishalisgre8";
+			$token = array(
+				"secret" => "IronMan",
+			);
+			$jwt = JWT::encode($token, $key);
+			$res = $id->$jwt;
+			echo json_encode($res);
+		}
 	}
 
 ?>
